2a17e00576ce89e4cb014e876efb7705b94b90f1
tim <tjhickey@brandeis.edu>
2023-06-27 08:36:23 -0400

    added jupyter folder and courses.csv

---
+++ code/jupyter/people.py
@@ -0,0 +1,48 @@
+people = [
+{'name':'Tim',       'age':67,    'height':68},
+{'name':'Ryan' ,   'age':24,    'height':68},
+{'name':'Caitlin',   'age':28,     'height':64},
+{'name':'David',   'age':19,    'height':59},
+{'name':'Toby',     'age':18,    'height':59},
+{'name':'Anna',    'age':18,    'height':57},
+{'name':'Jason',   'age':20,    'height':68},
+{'name':'Jennifer','age':20,    'height':64},
+{'name':'Penny',   'age':22,    'height':64},
+
+{'name':'Sara', 'age':33, 'height':65},
+{'name':'Greg', 'age':45 , 'height':72},
+{'name':'Julian' , 'age':17, 'height':67},
+{'name':'Lebron',   'age': 38,'height':81},
+{'name':'Jayson',   'age': 25,'height':80},
+{'name':'Novak',    'age': 36,'height':74},
+{'name': 'Mike',   'age':42,   'height':72},
+{'name': 'Kayla',   'age':16,   'height':62},
+{'name': 'Kendrick',  'age':29,  'height':76},
+{"name":"Aria", "age":20, "height":68},
+{"name":"Emma", "age":18, "height":65},
+{"name":"Sophia", "age":60, "height":80}, 
+{'name':'John', 'age': 54,'height':91},
+{'name':'Joe The Giraffe','age': 23,'height':334},
+{'name':'Bob','age': 21,'height':67},
+{'name': 'Louis', 'age': 31, 'height': 67},
+{'name': 'Luci', 'age': 90, 'height': 80},
+{'name': 'Ruth', 'age': 66, 'height': 54},
+
+{'name' : 'Josh', 'age':29, 'height':87},
+{'name' : 'Helen', 'age':29, 'height':84},
+{'name' : 'Sarah', 'age':20, 'height':3}, 
+
+
+{'name':'Jomothy',    'age':29,    'height':59},
+{'name':'Piter',          'age':54,    'height':70},
+{'name':'Limmy',       'age':9,      'height':53},
+
+
+{'name': 'Sam', 'age': 20,  'height': 65},
+{'name': 'Cass', 'age': 21,  'height': 66},
+{'name': 'Jim', 'age': 19,  'height': 72}
+
+
+]  # end of people list
+
+print(len(people))
