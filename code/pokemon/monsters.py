23912c71ac91e0ae1d7cb2cd0f00aea9c7e500c7
Timothy J Hickey <tjhickey@brandeis.edu>
2025-06-23 07:38:49 -0400

    Create monsters.py

---
+++ code/pokemon/monsters.py
@@ -0,0 +1,34 @@
+available_monsters = [
+        {
+            'name': "Charmander",
+            'health': 100,
+            'attacks': [
+                {'name': 'Scratch', 'min_damage': 15, 'max_damage': 25},
+                {'name': 'Ember', 'min_damage': 10, 'max_damage': 30}
+            ]
+        },
+        {
+            'name': "Squirtle",
+            'health': 90,
+            'attacks': [
+                {'name': 'Tackle', 'min_damage': 10, 'max_damage': 20},
+                {'name': 'Water Gun', 'min_damage': 15, 'max_damage': 25}
+            ]
+        },
+        {
+            'name': "Bulbasaur",
+            'health': 110,
+            'attacks': [
+                {'name': 'Vine Whip', 'min_damage': 12, 'max_damage': 22},
+                {'name': 'Razor Leaf', 'min_damage': 8, 'max_damage': 35} # Example of wider range
+            ]
+        },
+        {
+            'name': "Pikachu",
+            'health': 85,
+            'attacks': [
+                {'name': 'Quick Attack', 'min_damage': 18, 'max_damage': 28},
+                {'name': 'Thunder Shock', 'min_damage': 13, 'max_damage': 32}
+            ]
+        }
+    ]
